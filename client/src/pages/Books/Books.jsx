9511291d8644e238dab4876751617876cee41f08
Irad Doron <irad.doron@gmail.com>
2022-06-07 21:07:48 +0300

    add accordion

--- client/src/pages/Books/Books.jsx
+++ client/src/pages/Books/Books.jsx
@@ -11,6 +11,7 @@ import books_data from '../../helpers/books_data/books_data';
 
 // imports of material ui
 import { Button, ButtonGroup, Typography } from '@mui/material';
+import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
 
 // styled components
 
@@ -31,79 +32,92 @@ function Books() {
   return (
     <>
       <PageHeader>עמוד הספרים</PageHeader>
-      <h2>קטגוריה: {books_data[categoryUrl]?.name}</h2>
-      <nav>
-        <ButtonGroup variant="outlined" sx={{ textDecoration: 'none' }}>
-          {getCategoriesNames().map((categoryUrlName, index) => {
-            return <GenericButton key={index} content={books_data[categoryUrlName]?.name} url={categoryUrlName} />;
-          })}
-        </ButtonGroup>
-      </nav>
-      {categoryUrl ? (
-        <>
-          <h2>ספר: {books_data[categoryUrl]?.books[bookUrl]?.name}</h2>
+      <Accordion>
+        <AccordionSummary>
+          <Typography>קטגוריות וספרים</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <h2>קטגוריה: {books_data[categoryUrl]?.name}</h2>
           <nav>
             <ButtonGroup variant="outlined" sx={{ textDecoration: 'none' }}>
-              {getBooksNames(categoryUrl)?.map((bookUrlName, index) => {
-                return (
-                  <GenericButton
-                    key={index}
-                    content={books_data[categoryUrl]?.books[bookUrlName].name}
-                    url={`${categoryUrl}/${bookUrlName}/0/0`}
-                  />
-                );
+              {getCategoriesNames().map((categoryUrlName, index) => {
+                return <GenericButton key={index} content={books_data[categoryUrlName]?.name} url={categoryUrlName} />;
               })}
             </ButtonGroup>
           </nav>
-        </>
-      ) : null}
+          {categoryUrl ? (
+            <>
+              <h2>ספר: {books_data[categoryUrl]?.books[bookUrl]?.name}</h2>
+              <nav>
+                <ButtonGroup variant="outlined" sx={{ textDecoration: 'none' }}>
+                  {getBooksNames(categoryUrl)?.map((bookUrlName, index) => {
+                    return (
+                      <GenericButton
+                        key={index}
+                        content={books_data[categoryUrl]?.books[bookUrlName].name}
+                        url={`${categoryUrl}/${bookUrlName}/0/0`}
+                      />
+                    );
+                  })}
+                </ButtonGroup>
+              </nav>
+            </>
+          ) : null}
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary>
+          <Typography>פרקים ושיעורים</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          {categoryUrl && bookUrl ? (
+            <>
+              <h2>
+                פרק מספר {chapterIndex} -
+                {books_data[categoryUrl]?.books[bookUrl]?.chapters[Number(chapterIndex)]?.chapterHeader}
+              </h2>
+              <nav>
+                <ul>
+                  {getChapterNames(categoryUrl, bookUrl)?.map((chapterIndexName, index) => {
+                    return (
+                      <li key={index}>
+                        <NavLink to={`${categoryUrl}/${bookUrl}/${index}/0`}>
+                          <Typography>{chapterIndexName}</Typography>
+                        </NavLink>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </nav>
+            </>
+          ) : null}
 
-      {categoryUrl && bookUrl ? (
-        <>
-          <h2>
-            פרק מספר {chapterIndex} -
-            {books_data[categoryUrl]?.books[bookUrl]?.chapters[Number(chapterIndex)]?.chapterHeader}
-          </h2>
-          <nav>
-            <ul>
-              {getChapterNames(categoryUrl, bookUrl)?.map((chapterIndexName, index) => {
-                return (
-                  <li key={index}>
-                    <NavLink to={`${categoryUrl}/${bookUrl}/${index}/0`}>
-                      <Typography>{chapterIndexName}</Typography>
-                    </NavLink>
-                  </li>
-                );
-              })}
-            </ul>
-          </nav>
-        </>
-      ) : null}
-
-      {categoryUrl && bookUrl && chapterIndex ? (
-        <>
-          <h2>
-            שיעור מספר {Number(lessonIndex) + 1} -
-            {
-              books_data[categoryUrl]?.books[bookUrl]?.chapters[Number(chapterIndex)]?.lessons[lessonIndex]
-                ?.lessonHeader
-            }
-          </h2>
-          <nav>
-            <ul>
-              {getLessonsNames(categoryUrl, bookUrl, chapterIndex)?.map((lessonIndexName, index) => {
-                return (
-                  <li key={index}>
-                    <NavLink to={`${categoryUrl}/${bookUrl}/${chapterIndex}/${index}`}>
-                      <Typography>{lessonIndexName}</Typography>
-                    </NavLink>
-                  </li>
-                );
-              })}
-            </ul>
-          </nav>
-        </>
-      ) : null}
+          {categoryUrl && bookUrl && chapterIndex ? (
+            <>
+              <h2>
+                שיעור מספר {Number(lessonIndex) + 1} -
+                {
+                  books_data[categoryUrl]?.books[bookUrl]?.chapters[Number(chapterIndex)]?.lessons[lessonIndex]
+                    ?.lessonHeader
+                }
+              </h2>
+              <nav>
+                <ul>
+                  {getLessonsNames(categoryUrl, bookUrl, chapterIndex)?.map((lessonIndexName, index) => {
+                    return (
+                      <li key={index}>
+                        <NavLink to={`${categoryUrl}/${bookUrl}/${chapterIndex}/${index}`}>
+                          <Typography>{lessonIndexName}</Typography>
+                        </NavLink>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </nav>
+            </>
+          ) : null}
+        </AccordionDetails>
+      </Accordion>
 
       <Outlet />
     </>
